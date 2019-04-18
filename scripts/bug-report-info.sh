d26914ae11490df73be40692573aca99ad555c53
Julien Olivain <juju@cotds.org>
2019-04-18 22:40:19 +0200

    scripts: bug-report-info.sh: execute commands only if found
    Signed-off-by: Julien Olivain <juju@cotds.org>

--- scripts/bug-report-info.sh
+++ scripts/bug-report-info.sh
@@ -1,25 +1,49 @@
 #! /bin/sh
 
-echo
-echo "uname:"
-uname -a
+if [ -x "$(command -v uname)" ] ; then
+    echo
+    echo "uname:"
+    uname -a
+fi
 
-echo
-echo "lsb_release:"
-lsb_release -a
+if [ -x "$(command -v lsb_release)" ] ; then
+    echo
+    echo "lsb_release:"
+    lsb_release -a
+fi
 
-echo
-echo "getconf:"
-getconf -a
+if [ -x "$(command -v getconf)" ] ; then
+    echo
+    echo "getconf:"
+    getconf -a
+fi
 
-echo
-echo "/proc/cpuinfo:"
-cat /proc/cpuinfo
+if [ -e "/proc/cpuinfo" ] ; then
+    echo
+    echo "/proc/cpuinfo:"
+    cat /proc/cpuinfo
+fi
 
-echo
-echo "lspci:"
-lspci
+if [ -x "$(command -v lspci)" ] ; then
+    echo
+    echo "lspci:"
+    lspci
+fi
 
-echo
-echo "glxinfo:"
-glxinfo
+if [ -x "$(command -v glxinfo)" ] ; then
+    echo
+    echo "glxinfo:"
+    glxinfo
+fi
+
+if [ -x "$(command -v modetest)" ] ; then
+    echo
+    echo "DRM modetest:"
+    modetest
+fi
+
+if [ -x "$(command -v weston-info)" ] ; then
+    echo
+    echo "weston-info"
+    weston-info
+fi
