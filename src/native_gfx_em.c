3fb5ddd2739ad21237f3ac776e3ce13d79c1283f
Julien Olivain <ju.o@free.fr>
2020-11-24 19:17:34 +0100

    native_gfx: em: add support for Emscripten / WebAssembly
    Signed-off-by: Julien Olivain <ju.o@free.fr>

---
+++ src/native_gfx_em.c
@@ -0,0 +1,156 @@
+/*
+ * Emscripten / WebAssembly
+ */
+
+/*
+ * This program is distributer under the 2-clause BSD license.
+ * See at the end of this file for details.
+ */
+
+/*
+ * See Emscripten documentation:
+ * https://emscripten.org/
+ * https://emscripten.org/docs/porting/multimedia_and_graphics/EGL-Support-in-Emscripten.html
+ * https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <EGL/egl.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#endif
+
+#include "native_gfx.h"
+
+static const int default_em_win_width = 320;
+static const int default_em_win_height = 180;
+
+struct native_gfx_s
+{
+  int width;
+  int height;
+};
+
+char *
+native_gfx_get_name(void)
+{
+  return ("Emscripten");
+}
+
+native_gfx_t *
+native_gfx_open_display(void)
+{
+  native_gfx_t *gfx;
+
+  gfx = malloc(sizeof (*gfx));
+  if (gfx == NULL) {
+    fprintf(stderr,
+            "native_gfx_open_display(): Can't allocate memory: error %i: %s\n",
+            errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  memset(gfx, 0, sizeof (*gfx));
+
+  return (gfx);
+}
+
+NativeDisplayType
+native_gfx_get_egl_native_display(const native_gfx_t *gfx)
+{
+  GFX_UNUSED(gfx);
+  return (EGL_DEFAULT_DISPLAY);
+}
+
+NativeWindowType
+native_gfx_get_egl_native_window(const native_gfx_t *gfx)
+{
+  GFX_UNUSED(gfx);
+  return ((NativeWindowType)0);
+}
+
+void
+native_gfx_create_window(native_gfx_t *gfx, int width, int height, int xpos, int ypos)
+{
+  GFX_UNUSED(xpos);
+  GFX_UNUSED(ypos);
+
+  if (width == 0)
+    width = default_em_win_width;
+
+  if (height == 0)
+    height = default_em_win_height;
+
+  emscripten_set_canvas_element_size("canvas", width, height);
+
+  gfx->width = width;
+  gfx->height = height;
+}
+
+void
+native_gfx_destroy_window(native_gfx_t *gfx)
+{
+  GFX_UNUSED(gfx);
+}
+
+void
+native_gfx_close_display(native_gfx_t *gfx)
+{
+  free(gfx);
+}
+
+void
+native_gfx_swap_buffers(native_gfx_t *gfx)
+{
+  GFX_UNUSED(gfx);
+}
+
+int
+native_gfx_get_window_width(const native_gfx_t *gfx)
+{
+  return (gfx->width);
+}
+
+int
+native_gfx_get_window_height(const native_gfx_t *gfx)
+{
+  return (gfx->height);
+}
+
+/*
+* Copyright (c) 2015-2020, Julien Olivain <ju.o@free.fr>
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* End-of-File */
